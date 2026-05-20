2f9860c61d087aa3ed819fde74c52cc3d9e559a7
Qingsong Hu <kingsanhu@gmail.com>
2026-05-20 17:03:06 +0800

    Finish the first part of HW4
    Fetch student data and print in the console

--- src/components/Classroom.jsx
+++ src/components/Classroom.jsx
@@ -1,27 +1,42 @@
+import { useEffect, useState } from "react";
 import { Button, Container, Form, Row } from "react-bootstrap";
 
 const Classroom = () => {
-    return <div>
-        <h1>Badger Book</h1>
-        <p>Search for students below!</p>
-        <hr />
-        <Form>
-            <Form.Label htmlFor="searchName">Name</Form.Label>
-            <Form.Control id="searchName"/>
-            <Form.Label htmlFor="searchMajor">Major</Form.Label>
-            <Form.Control id="searchMajor"/>
-            <Form.Label htmlFor="searchInterest">Interest</Form.Label>
-            <Form.Control id="searchInterest"/>
-            <br />
-            <Button variant="neutral">Reset Search</Button>
-        </Form>
-        <Container fluid>
-            <Row>
-                { /* TODO Students go here! */ }
-            </Row>
-        </Container>
-    </div>
+    const [stu, setStu] = useState([]);
 
-}
+    useEffect(() => {
+        fetch("https://cs571.org/rest/s25/hw4/students", {
+            headers: {
+                "X-CS571-ID": CS571.getBadgerId(),
+            },
+        })
+            .then((req) => req.json())
+            .then((data) => {
+                setStu(data);
+                console.log(data);
+            });
+    }, []);
+
+    return (
+        <div>
+            <h1>Badger Book</h1>
+            <p>Search for students below!</p>
+            <hr />
+            <Form>
+                <Form.Label htmlFor="searchName">Name</Form.Label>
+                <Form.Control id="searchName" />
+                <Form.Label htmlFor="searchMajor">Major</Form.Label>
+                <Form.Control id="searchMajor" />
+                <Form.Label htmlFor="searchInterest">Interest</Form.Label>
+                <Form.Control id="searchInterest" />
+                <br />
+                <Button variant="neutral">Reset Search</Button>
+            </Form>
+            <Container fluid>
+                <Row>{/* TODO Students go here! */}</Row>
+            </Container>
+        </div>
+    );
+};
 
 export default Classroom;
