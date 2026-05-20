22ee229358347dc336234c6106f363fa4594e229
Qingsong Hu <kingsanhu@gmail.com>
2026-05-20 17:23:50 +0800

    Finish part 5 of HW4
    Show other student data to the screen

--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -1,8 +1,22 @@
 const Student = (props) => {
-    return <div>
-        <h2>{props.name.first} {props.name.last}</h2>
-        {/* TODO Student data goes here! */}
-    </div>
-}
+    return (
+        <div>
+            <h2>
+                {props.name.first} {props.name.last}
+            </h2>
+            <strong>{props.major}</strong>
+            <p>
+                {props.name.first} is taking {props.numCredits} and is{" "}
+                {props.fromWisconsin ? "" : "NOT"} from Wisconsin
+            </p>
+            <p>They have {props.interests.length} interests including...</p>
+            <ul>
+                {props.interests.map((interest) => (
+                    <li key={props.id + interest}>{interest}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
 
 export default Student;
